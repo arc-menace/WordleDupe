4c1e3c1704f39693a95a42900d1f1604f71ce1f8
Joseph Hahn <jmhahn0@gmail.com>
2024-03-20 22:35:32 -0500

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/WordleDupe/",
+  base: "https://arc-menace.github.io/WordleDupe/",
   plugins: [vue()],
 })
