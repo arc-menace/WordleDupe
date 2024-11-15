5695c8b96add36c74ad31d0c13b26067fd5637aa
Joseph Hahn <jmhahn0@gmail.com>
2024-11-15 15:19:20 -0600

    disable double tap to zoom

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -20,41 +20,47 @@
     .top-row {
         margin-top: 2rem;
     }
+
+    .keyboard {
+        touch-action: manipulation;
+    }
 </style>
 
 <template>
-    <div class="row top-row">
-        <KeyboardKey pressValue="Q"/>
-        <KeyboardKey pressValue="W"/>
-        <KeyboardKey pressValue="E"/>
-        <KeyboardKey pressValue="R"/>
-        <KeyboardKey pressValue="T"/>
-        <KeyboardKey pressValue="Y"/>
-        <KeyboardKey pressValue="U"/>
-        <KeyboardKey pressValue="I"/>
-        <KeyboardKey pressValue="O"/>
-        <KeyboardKey pressValue="P"/>
-    </div>
-    <div class="row">
-        <KeyboardKey pressValue="A"/>
-        <KeyboardKey pressValue="S"/>
-        <KeyboardKey pressValue="D"/>
-        <KeyboardKey pressValue="F"/>
-        <KeyboardKey pressValue="G"/>
-        <KeyboardKey pressValue="H"/>
-        <KeyboardKey pressValue="J"/>
-        <KeyboardKey pressValue="K"/>
-        <KeyboardKey pressValue="L"/>
-    </div>
-    <div class="row">
-        <KeyboardKey pressValue="Enter"/>
-        <KeyboardKey pressValue="Z"/>
-        <KeyboardKey pressValue="X"/>
-        <KeyboardKey pressValue="C"/>
-        <KeyboardKey pressValue="V"/>
-        <KeyboardKey pressValue="B"/>
-        <KeyboardKey pressValue="N"/>
-        <KeyboardKey pressValue="M"/>
-        <KeyboardKey pressValue="Back"/>
-    </div>
+    <div class="keyboard">
+        <div class="row top-row">
+            <KeyboardKey pressValue="Q"/>
+            <KeyboardKey pressValue="W"/>
+            <KeyboardKey pressValue="E"/>
+            <KeyboardKey pressValue="R"/>
+            <KeyboardKey pressValue="T"/>
+            <KeyboardKey pressValue="Y"/>
+            <KeyboardKey pressValue="U"/>
+            <KeyboardKey pressValue="I"/>
+            <KeyboardKey pressValue="O"/>
+            <KeyboardKey pressValue="P"/>
+        </div>
+        <div class="row">
+            <KeyboardKey pressValue="A"/>
+            <KeyboardKey pressValue="S"/>
+            <KeyboardKey pressValue="D"/>
+            <KeyboardKey pressValue="F"/>
+            <KeyboardKey pressValue="G"/>
+            <KeyboardKey pressValue="H"/>
+            <KeyboardKey pressValue="J"/>
+            <KeyboardKey pressValue="K"/>
+            <KeyboardKey pressValue="L"/>
+        </div>
+        <div class="row">
+            <KeyboardKey pressValue="Enter"/>
+            <KeyboardKey pressValue="Z"/>
+            <KeyboardKey pressValue="X"/>
+            <KeyboardKey pressValue="C"/>
+            <KeyboardKey pressValue="V"/>
+            <KeyboardKey pressValue="B"/>
+            <KeyboardKey pressValue="N"/>
+            <KeyboardKey pressValue="M"/>
+            <KeyboardKey pressValue="Back"/>
+        </div>
+    </div>    
 </template>
